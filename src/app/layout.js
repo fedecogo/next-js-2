4341d7fa0426904ca2d4084a615618d2a0c38af9
fedecogo <141748020+fedecogo@users.noreply.github.com>
2024-07-29 21:24:20 +0200

    Suspense,RecipeDetails and Item

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Loading from "./loading";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Suspense fallback={<Loading/>}>{children}</Suspense>
+      </body>
+
     </html>
   );
 }
